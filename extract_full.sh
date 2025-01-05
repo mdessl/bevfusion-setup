c9d23ec31fa2b1c7b5e7fb356b2748a7cffb9772
essl <49913272+mdessl@users.noreply.github.com>
2025-01-05 01:08:51 +0100

    Update extract_full.sh

--- extract_full.sh
+++ extract_full.sh
@@ -9,7 +9,12 @@ cd /mmdet3d/data/nuscenes
 sudo apt-get install pigz  # On Debian/Ubuntu
 sudo apt install zip unzip
 for file in *.zip; do unzip "$file" && rm "$file"; done; for file in *.tar; do tar --use-compress-program=pigz --blocking-factor=512 -xvf "$file" && rm "$file"; done; mv expansion basemap prediction maps/
-
+if [ -f "nuScenes-lidarseg-all-v1.0.tar.bz2" ]; then
+    echo "Extracting lidarseg data..."
+    tar -xjf nuScenes-lidarseg-all-v1.0.tar.bz2 && rm nuScenes-lidarseg-all-v1.0.tar.bz2
+else
+    echo "Lidarseg file not found, skipping..."
+fi
 
 # Download and setup info files
 rm -f nuscenes_infos_train.pkl nuscenes_infos_val.pkl
